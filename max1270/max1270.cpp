45ac5a64e8ddd676bc9ffefab43073a72e197f12
Will Dickson <will@iorodeo.com>
2025-10-20 16:22:06 -0700

    Updated max1270 library.  Modified so that sstrb use is optional.

--- max1270/max1270.cpp
+++ max1270/max1270.cpp
@@ -1,45 +1,60 @@
 // ----------------------------------------------------------------------------
 // max1270.cpp
 //
-// Provides an SPI based interface to the MAX1270 multirange data acquisition IC
+// Provides an SPI based uint8_terface to the MAX1270 multirange data acquisition IC
 // from Maxim.
 //
 // Author: Will Dickson, IO Rodeo Inc.
 // ----------------------------------------------------------------------------
 #if defined(ARDUINO) && ARDUINO >= 100
-#include "Arduino.h"
+    #include "Arduino.h"
 #else
-#include "WProgram.h"
+    #include "WProgram.h"
 #endif
 #include "SPI.h"
 #include "max1270.h"
+#include "Streaming.h"
 
 // Definitions
-#define CLK_INTERNAL  0b00000000
-#define CLK_EXTERNAL  0b00000001
-#define RANGE_5V      0b00000000
-#define RANGE_10V     0b00001000
-#define BIPOLAR       0b00000100
-#define UNIPOLAR      0b00000000
-#define CTL_BYTE_INIT 0b10000000
-#define SSTRB_WAIT_US 1
+const uint8_t CLK_INTERNAL  = 0b00000000;
+const uint8_t CLK_EXTERNAL  = 0b00000001;
+const uint8_t RANGE_5V      = 0b00000000;
+const uint8_t RANGE_10V     = 0b00001000;
+const uint8_t BIPOLAR       = 0b00000100;
+const uint8_t UNIPOLAR      = 0b00000000;
+const uint8_t CTL_BYTE_INIT = 0b10000000;
+
+const uint32_t NO_SSTRB_CONVERSION_WAIT_US = 20;
 
 // LSB to voltages for the different gain and polarity combinations
-#define LSB2VOLTS_5V_BIPOLAR    0.00244140625
-#define LSB2VOLTS_5V_UNIPOLAR   0.001220703125
-#define LSB2VOLTS_10V_BIPOLAR   0.0048828125
-#define LSB2VOLTS_10V_UNIPOLAR  0.00244140625
+const float LSB2VOLTS_5V_BIPOLAR   = 0.00244140625;
+const float LSB2VOLTS_5V_UNIPOLAR  = 0.001220703125;
+const float LSB2VOLTS_10V_BIPOLAR  = 0.0048828125;
+const float LSB2VOLTS_10V_UNIPOLAR = 0.00244140625;
+
 
 // ----------------------------------------------------------------------------
 // MAX1270::MAX1270
 //
-// Constructor
+// Constructor - when not using sstrb
 // ----------------------------------------------------------------------------
-MAX1270::MAX1270(int csPin, int sstrbPin) {
-  mode = 0;
-  // Initialize digital IO
-  cs = csPin;
-  sstrb = sstrbPin;
+MAX1270::MAX1270(uint8_t cs_pin) {
+    mode_ = 0;
+    cs_ = cs_pin;
+    use_sstrb_ = false;
+}
+
+// ----------------------------------------------------------------------------
+// MAX1270::MAX1270
+//
+// Constructor - when using sstrb
+// ----------------------------------------------------------------------------
+
+MAX1270::MAX1270(uint8_t cs_pin, uint8_t sstrb) {
+    mode_ = 0;
+    cs_ = cs_pin;
+    sstrb_ = sstrb;
+    use_sstrb_ = true;
 }
 
 // ---------------------------------------------------------------------------
@@ -47,243 +62,304 @@ MAX1270::MAX1270(int csPin, int sstrbPin) {
 // ----------------------------------------------------------------------------
 
 void MAX1270::initialize() {
-  pinMode(cs,OUTPUT);
-  pinMode(sstrb, INPUT);
-  digitalWrite(cs,HIGH);
-  // Set default polarity, range and clock
-  setBipolar();
-  setRange10V();
-  setInternalClock();
+    pinMode(cs_,OUTPUT);
+    digitalWrite(cs_,HIGH);
+    if (use_sstrb_) {
+        pinMode(sstrb_, INPUT);
+    }
+    // Set default polarity, range and clock
+    set_bipolar();
+    set_range_10V();
+    set_internal_clock();
 }
+
 
 // ----------------------------------------------------------------------------
 // MAX1270::sample
 //
 // Returns an analog sample for the given channel.
 // ----------------------------------------------------------------------------
-int MAX1270::sample(int chan) {
-  int ctlByte;
-  int sample;
-  int msb;
-  int lsb;
-  int sgn;
+int16_t MAX1270::sample(uint8_t chan) {
+    uint8_t ctl_byte;
+    uint16_t msb;
+    uint16_t lsb;
+    uint16_t sgn;
+    int16_t sample;
 
-  // Enable chip SPI communications
-  digitalWrite(cs,LOW);
+    // Enable chip SPI communications
+    SPI.beginTransaction(SPISettings(spi_freq_, MSBFIRST, SPI_MODE0));
+    digitalWrite(cs_,LOW);
 
-  // Transfer control byte
-  ctlByte = getCtlByte(chan);
-  SPI.transfer(ctlByte);
+    uint16_t count_hi = 0;
+    uint16_t count_lo = 0;
 
-  // Wait for sstrb to go high
-  while(digitalRead(sstrb) == LOW) {};
+    // Transfer control byte
+    ctl_byte = get_ctl_byte(chan);
+    SPI.transfer(ctl_byte);
 
-  // Read sample
-  msb = SPI.transfer(0x00);
-  lsb = SPI.transfer(0x00);
+    if (use_sstrb_) {
+        // Wait for sstrb to go low and then high again
+        while(digitalRead(sstrb_) == HIGH) {}; 
+        while(digitalRead(sstrb_) == LOW)  {}; 
+    }
+    else {
+        delayMicroseconds(NO_SSTRB_CONVERSION_WAIT_US);
+    }
 
-  // Convert sample from 12bit to 16bit number
-  lsb = lsb >> 4;
-  sgn = (0b10000000 & msb) >> 7;
-  msb = msb << 4;
-  if (sgn == 1) {
-    msb = 0b1111000000000000 | msb;
-  }
-  sample = msb | lsb;
+    // Read sample
+    msb = SPI.transfer(0x00);
+    lsb = SPI.transfer(0x00);
 
-  // Disable chip SPI communications
-  digitalWrite(cs,HIGH);
+    // Convert sample from 12bit to 16bit number
+    lsb = lsb >> 4;
+    sgn = (0b10000000 & msb) >> 7;
+    msb = msb << 4;
+    if (sgn == 1) {
+        msb = 0b1111000000000000 | msb;
+    }
+    sample = msb | lsb;
 
-  return sample;
+    // Disable chip SPI communications
+    digitalWrite(cs_,HIGH);
+    SPI.endTransaction();
+
+    return sample;
 }
 
 // ----------------------------------------------------------------------------
-// MAX1270::sampleAll
+// MAX1270::sample_all
 //
 // Gets an analog sample from all input channels
 // ----------------------------------------------------------------------------
-void MAX1270::sampleAll(int values[] ) {
-  for (int i=0; i<MAX1270_NUMCHAN; i++) {
-    values[i] = sample(i);
-  }
+void MAX1270::sample_all(int16_t values[] ) {
+    for (uint8_t i=0; i<MAX1270_NUMCHAN; i++) {
+        values[i] = sample(i);
+    }
 }
 
 
 // ----------------------------------------------------------------------------
-// MAX1270::sampleVolts
+// MAX1270::sample_volts
 //
-// Gets an analog sample from the given channel and converts to a floating point
+// Gets an analog sample from the given channel and converts to a floating pouint8_t
 // voltage value
 //
-// Note, may want to move this to fixed point instead floating point.
+// Note, may want to move this to fixed pouint8_t instead floating pouint8_t.
 // ----------------------------------------------------------------------------
-float MAX1270::sampleVolts(int chan) {
-  int valueInt;
-  float valueVolts = 0;
-  if ((chan >= 0) && (chan < MAX1270_NUMCHAN)) {
-    valueInt = sample(chan);
-    switch (chanRange[chan] | chanPolarity[chan]) {
+float MAX1270::sample_volts(uint8_t chan) {
+    int16_t value_int;
+    float value_volts = 0;
+    if ((chan >= 0) && (chan < MAX1270_NUMCHAN)) {
+        value_int = sample(chan);
+        switch (chan_range_[chan] | chan_polarity_[chan]) {
 
-    case (RANGE_5V | BIPOLAR):
-      valueVolts = LSB2VOLTS_5V_BIPOLAR*valueInt;
-      break;
+            case (RANGE_5V | BIPOLAR):
+                value_volts = LSB2VOLTS_5V_BIPOLAR*value_int;
+                break;
 
-    case (RANGE_5V | UNIPOLAR):
-      valueVolts = LSB2VOLTS_5V_UNIPOLAR*valueInt;
-      break;
+            case (RANGE_5V | UNIPOLAR):
+                value_volts = LSB2VOLTS_5V_UNIPOLAR*value_int;
+                break;
 
-    case (RANGE_10V | BIPOLAR):
-      valueVolts = LSB2VOLTS_10V_BIPOLAR*valueInt;
-      break;
+            case (RANGE_10V | BIPOLAR):
+                value_volts = LSB2VOLTS_10V_BIPOLAR*value_int;
+                break;
 
-    case (RANGE_10V | UNIPOLAR):
-      valueVolts = LSB2VOLTS_10V_UNIPOLAR*valueInt;
-      break;
+            case (RANGE_10V | UNIPOLAR):
+                value_volts = LSB2VOLTS_10V_UNIPOLAR*value_int;
+                break;
 
-    default:
-      break;
+            default:
+                break;
+        }
     }
-  }
-  return valueVolts;
+    return value_volts;
 }
 
 // ----------------------------------------------------------------------------
-// MAX1270::sampleAllVolts
+// MAX1270::sample_all_volts
 //
 // Gets an analog sample from all input channels and converts to a floating
-// point voltage value.
+// pouint8_t voltage value.
 //
-// Note, may want to move this to fixed point instead of floating point.
+// Note, may want to move this to fixed pouint8_t instead of floating pouint8_t.
 // ----------------------------------------------------------------------------
-void MAX1270::sampleAllVolts(float values[] ) {
-  for (int i=0; i<MAX1270_NUMCHAN; i++) {
-    values[i] = sampleVolts(i);
-  }
+void MAX1270::sample_all_volts(float values[] ) {
+    for (uint8_t i=0; i<MAX1270_NUMCHAN; i++) {
+        values[i] = sample_volts(i);
+    }
 }
 // ----------------------------------------------------------------------------
-// MAX1270::setInternalClock
+// MAX1270::set_internal_clock
 //
-// Set the MAX1270 to use its internal clock
+// Set the MAX1270 to use its uint8_ternal clock
 // ----------------------------------------------------------------------------
-void MAX1270::setInternalClock() {
-  mode &= 0b10;
-  mode |= CLK_INTERNAL;
+void MAX1270::set_internal_clock() {
+    mode_ &= 0b10;
+    mode_ |= CLK_INTERNAL;
 }
 
 // ----------------------------------------------------------------------------
-// MAX1270::setExternalClock
+// MAX1270::set_external_clock
 //
 // Set the MAX1270 to use an external clock source.
 // ----------------------------------------------------------------------------
-void MAX1270::setExternalClock() {
-  mode &= 0b10;
-  mode |= CLK_EXTERNAL;
+void MAX1270::set_external_clock() {
+    mode_ &= 0b10;
+    mode_ |= CLK_EXTERNAL;
 }
 
 // ----------------------------------------------------------------------------
-// MAX1270::setBipolar
+// MAX1270::set_bipolar
 //
 // Set given the analog input channel to bipolar operation
 // ----------------------------------------------------------------------------
-void MAX1270::setBipolar(int chan) {
-  if ((chan >=0) && (chan < MAX1270_NUMCHAN)) {
-    chanPolarity[chan] = BIPOLAR;
-  }
+void MAX1270::set_bipolar(uint8_t chan) {
+    if ((chan >=0) && (chan < MAX1270_NUMCHAN)) {
+        chan_polarity_[chan] = BIPOLAR;
+    }
 }
 
 // ----------------------------------------------------------------------------
-// MAX1270::setBipolar
+// MAX1270::set_bipolar
 //
 // Set all analog input channels to bipolar operation
 // ----------------------------------------------------------------------------
-void MAX1270::setBipolar() {
-  for (int i=0; i<MAX1270_NUMCHAN;i++) {
-    setBipolar(i);
-  }
+void MAX1270::set_bipolar() {
+    for (uint8_t i=0; i<MAX1270_NUMCHAN;i++) {
+        set_bipolar(i);
+    }
 }
 
 // ----------------------------------------------------------------------------
-// MAX1270::setUnipolar
+// MAX1270::set_unipolar
 //
 // Set the given analog input channel to unipolar operation
 // ----------------------------------------------------------------------------
-void MAX1270::setUnipolar(int chan) {
-  if ((chan >= 0) && (chan < MAX1270_NUMCHAN)) {
-    chanPolarity[chan] = UNIPOLAR;
-  }
+void MAX1270::set_unipolar(uint8_t chan) {
+    if ((chan >= 0) && (chan < MAX1270_NUMCHAN)) {
+        chan_polarity_[chan] = UNIPOLAR;
+    }
 }
 
 // ----------------------------------------------------------------------------
-// MAX1270::setUnipolar
+// MAX1270::set_unipolar
 //
 // Set all analog input channels to unipolar operation
 // ----------------------------------------------------------------------------
-void MAX1270::setUnipolar() {
-  for (int i=0; i<MAX1270_NUMCHAN;i++) {
-    setUnipolar(i);
-  }
+void MAX1270::set_unipolar() {
+    for (uint8_t i=0; i<MAX1270_NUMCHAN;i++) {
+        set_unipolar(i);
+    }
 }
 
 // ---------------------------------------------------------------------------
-// MAX1270::setRange10V
+// MAX1270::set_range_10V
 //
 // Sets the range of the given analog input channel to 10V. Note, if polarity
 // is set to bipolar than range will be (-10V, 10V) and if polarity is set to
 // unipolar than range will be (0,10V).
 // ---------------------------------------------------------------------------
-void MAX1270::setRange10V(int chan) {
-  if ((chan >= 0) && (chan < MAX1270_NUMCHAN)) {
-    chanRange[chan] = RANGE_10V;
-  }
+void MAX1270::set_range_10V(uint8_t chan) {
+    if ((chan >= 0) && (chan < MAX1270_NUMCHAN)) {
+        chan_range_[chan] = RANGE_10V;
+    }
 }
 
 // --------------------------------------------------------------------------
-// MAX1270::setRange10V
+// MAX1270::set_range_10V
 //
 // Sets the range of all analog inputs to 10V.  Note, if polarity  is set to
 // bipolar than range will be (-10V, 10V) and if polarity is set to unipolar
 // than range will be (0,10V).
 // --------------------------------------------------------------------------
-void MAX1270::setRange10V() {
-  for (int i=0; i<MAX1270_NUMCHAN; i++) {
-    setRange10V(i);
-  }
+void MAX1270::set_range_10V() {
+    for (uint8_t i=0; i<MAX1270_NUMCHAN; i++) {
+        set_range_10V(i);
+    }
 }
 
 // ----------------------------------------------------------------------------
-// MAX1270::setRange5V
+// MAX1270::set_range_5V
 //
 // Sets the range of the given analog input channel to 5V. Note, if polarity is
 // set to bipolar than range will be (-5V, 5V) and if polarity is set to unipolar
 // than range will be (0,5V).
 // ----------------------------------------------------------------------------
-void MAX1270::setRange5V(int chan) {
-  if ((chan >= 0) && (chan < MAX1270_NUMCHAN)) {
-    chanRange[chan] = RANGE_5V;
-  }
+void MAX1270::set_range_5V(uint8_t chan) {
+    if ((chan >= 0) && (chan < MAX1270_NUMCHAN)) {
+        chan_range_[chan] = RANGE_5V;
+    }
 }
 
 // ----------------------------------------------------------------------------
-// MAX1270::setRange5V
+// MAX1270::set_range_5V
 //
 // Sets the range of all analog inputs to 5V. Note, if polarity  is set to
 // bipolar than range will be (-5V, 5V) and if polarity is set to unipolar
 // than range will be (0,5V).
 // ----------------------------------------------------------------------------
-void MAX1270::setRange5V() {
-  for (int i=0; i<MAX1270_NUMCHAN; i++) {
-    setRange5V(i);
-  }
+void MAX1270::set_range_5V() {
+    for (uint8_t i=0; i<MAX1270_NUMCHAN; i++) {
+        set_range_5V(i);
+    }
 }
 
 // ----------------------------------------------------------------------------
-// MAX1270:numchan
+// MAX1270::num_chan
 //
 // Returns the number of analog input channels. Note, this is the same as the
 // definition MAX1270_NUMCHAN.
 // ---------------------------------------------------------------------------
-int MAX1270::numChan() {
-  return MAX1270_NUMCHAN;
+uint8_t MAX1270::num_chan() {
+    return MAX1270_NUMCHAN;
+}
+
+// ----------------------------------------------------------------------------
+// MAX1270::set_use_sstrb
+//
+// Sets whether or not the sstrb pin should be used to determine when a 
+// converions is complete.
+// ---------------------------------------------------------------------------
+void MAX1270::set_use_sstrb(bool value) {
+    use_sstrb_ = value;
+
+}
+
+// ----------------------------------------------------------------------------
+// MAX1270::set_use_sstrb
+//
+// Returns the current use_sstrb setting. 
+// ---------------------------------------------------------------------------
+bool MAX1270::get_use_sstrb() {
+    return use_sstrb_;
+}
+
+// ----------------------------------------------------------------------------
+// MAX1270::set_use_sstrb
+//
+// Returns the sstrb pin number
+// ---------------------------------------------------------------------------
+uint8_t MAX1270::sstrb_pin() {
+    return sstrb_;
+}
+
+// ----------------------------------------------------------------------------
+// MAX1270::set_spi_freq
+//
+// Set the SPI transfer frequency
+// ---------------------------------------------------------------------------
+void MAX1270::set_spi_freq(uint32_t spi_freq) {
+    spi_freq_ = spi_freq;
+}
+
+// ----------------------------------------------------------------------------
+// MAX1270::get_spi_freq
+//
+// Get the current value for the SPI transfere frequency
+// ---------------------------------------------------------------------------
+uint32_t MAX1270::get_spi_freq() {
+    return spi_freq_;
 }
 
 // ----------------------------------------------------------------------------
@@ -291,31 +367,33 @@ int MAX1270::numChan() {
 //-----------------------------------------------------------------------------
 
 // ----------------------------------------------------------------------------
-// MAX1270::getChanMask
+// MAX1270::get_chan_mask
 //
 // Returns control byte channel selection mask. Bits 4,5, and 6 of the control
 // byte select the analog input channel.
 // ----------------------------------------------------------------------------
-int MAX1270::getChanMask(int chan) {
-  int mask = 0;
-  if ((chan >= 0) && (chan < MAX1270_NUMCHAN)) {
-    mask = chan << 4;
-  }
-  return mask;
+uint8_t MAX1270::get_chan_mask(uint8_t chan) {
+    uint8_t mask = 0;
+    if ((chan >= 0) && (chan < MAX1270_NUMCHAN)) {
+        mask = chan << 4;
+    }
+    return mask;
 }
 
 // ---------------------------------------------------------------------------
-// MAX1270::getCtlByte
+// MAX1270::get_ctl_byte
 //
 // Returns the control byte for the given channel based on the current settings
 // such as range, polarity, clock selection.
 // ----------------------------------------------------------------------------
-int MAX1270::getCtlByte(int chan) {
-  int ctlByte = CTL_BYTE_INIT;
-  ctlByte |= getChanMask(chan);
-  ctlByte |= chanRange[chan];
-  ctlByte |= chanPolarity[chan];
-  ctlByte |= mode;
-  return ctlByte;
+uint8_t MAX1270::get_ctl_byte(uint8_t chan) {
+    uint8_t ctl_byte = CTL_BYTE_INIT;
+    ctl_byte |= get_chan_mask(chan);
+    ctl_byte |= chan_range_[chan];
+    ctl_byte |= chan_polarity_[chan];
+    ctl_byte |= mode_;
+    return ctl_byte;
 }
+
+
 
--- max1270/max1270.h
+++ max1270/max1270.h
@@ -1,7 +1,7 @@
 // ----------------------------------------------------------------------------
 // max1270.h
 //
-// Provides an SPI based interface to the MAX1270 multirange data acquisition IC
+// Provides an SPI based uint16_terface to the MAX1270 multirange data acquisition IC
 // from Maxim.
 //
 // Author: Will Dickson, IO Rodeo Inc.
@@ -9,35 +9,59 @@
 #ifndef _MAX1270_H_
 #define _MAX1270_H_
 
-#define MAX1270_NUMCHAN 8
+constexpr uint8_t MAX1270_NUMCHAN = 8;
 
 class MAX1270 {
- private:
-  int cs;                            // Chip select DIO
-  int sstrb;                         // Sample strobe DIO
-  int chanRange[MAX1270_NUMCHAN];    // Channel Ranges
-  int chanPolarity[MAX1270_NUMCHAN]; // Channel Polarity
-  int mode;                          // Clock selection
-  int getChanMask(int chan);
-  int getCtlByte(int chan);
- public:
-  MAX1270(int csPin, int sstrbPin);
-  void setBipolar();
-  void setBipolar(int chan);
-  void setUnipolar();
-  void setUnipolar(int chan);
-  void setRange5V();
-  void setRange5V(int chan);
-  void setRange10V();
-  void setRange10V(int chan);
-  void setInternalClock();
-  void setExternalClock();
-  void initialize();
-  int  sample(int chan);
-  void sampleAll(int value[]);
-  float sampleVolts(int chan);
-  void sampleAllVolts(float value[]);
-  int numChan();
+
+    public:
+
+        MAX1270(uint8_t csPin);
+        MAX1270(uint8_t csPin, uint8_t sstrb);
+
+        void initialize();
+
+        void set_spi_freq(uint32_t spi_freq);
+        uint32_t get_spi_freq();
+
+        void set_bipolar();
+        void set_bipolar(uint8_t chan);
+
+        void set_unipolar();
+        void set_unipolar(uint8_t chan);
+
+        void set_range_5V();
+        void set_range_5V(uint8_t chan);
+
+        void set_range_10V();
+        void set_range_10V(uint8_t chan);
+        
+        void set_internal_clock();
+        void set_external_clock();
+
+        int16_t  sample(uint8_t chan);
+        void sample_all(int16_t value[]);
+
+        float sample_volts(uint8_t chan);
+        void sample_all_volts(float value[]);
+
+        uint8_t num_chan();
+
+        void set_use_sstrb(bool value);
+        bool get_use_sstrb();
+        uint8_t sstrb_pin();
+
+    private:
+        uint8_t cs_ = 2;                 // Chip select DIO
+        uint8_t sstrb_ = 4;              // Sample strobe DIO pin
+        bool use_sstrb_ = false;         // Whether or not sstrb is used
+        uint32_t spi_freq_ = 2000000;  // SPI frequency 
+                                                  //
+        uint8_t chan_range_[MAX1270_NUMCHAN];     // Channel Ranges
+        uint8_t chan_polarity_[MAX1270_NUMCHAN];  // Channel Polarity
+        uint8_t mode_;                            // Clock selection
+                                                
+        uint8_t get_chan_mask(uint8_t chan);
+        uint8_t get_ctl_byte(uint8_t chan);
 };
 
 #endif
